7843c82eb4b8841bd0909bda7417870a7d8c320a
teffcode <tefy.1@live.com>
2017-09-27 18:34:19 -0500

    📝 AddTodo Component

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,7 +1,38 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
+import { addTodo } from '../actions';
 
-const AddTodo = () => <div>Add Todo</div>;
+/*
+ * DISPATCH: Only way to make a change in the state
+ */
+
+const AddTodo = ({ dispatch }) => {
+  let input;
+
+  return (
+    <div>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          if (!input.value.trim()) {
+            return;
+          }
+          dispatch(addTodo(input.value));
+          input.value = '';
+        }}
+      >
+        <input ref={node => { input = node; }} />
+        <button type="submit">
+          Add Todo
+        </button>
+      </form>
+    </div>
+  );
+};
+
+AddTodo.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+};
 
 /* 
  * CONNECT: To map the stores state and dispatch to the props of a component
