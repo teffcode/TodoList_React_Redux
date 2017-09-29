2a946e2c3e962b5fcbf64c70468545c711765259
teffcode <tefy.1@live.com>
2017-09-29 09:36:17 -0500

    💎 Todos Reducer

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -10,3 +10,17 @@ const todo = (state, action) => {
             return state;
     }
 };
+
+const todos = (state = [], action) => {
+    switch(action.type){
+        case 'ADD_TODO':
+            return [
+                ...state,
+                todo(undefined, action)
+            ];
+        default:
+            return state;
+    }
+}
+
+export default todos;
